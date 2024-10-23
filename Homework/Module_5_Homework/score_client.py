64f5012dabd81cd888c9a72bf9c923724d46e16d
hugi-codes <mindthehug@googlemail.com>
2024-10-23 11:18:28 +0000

    Add client script to score predictions from Flask web service

---
+++ Homework/Module_5_Homework/score_client.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://localhost:5000/predict"  # Update this URL if hosted on a different machine
+client = {"job": "student", "duration": 280, "poutcome": "failure"}
+
+response = requests.post(url, json=client)
+
+# Check if the request was successful and print the result
+if response.status_code == 200:
+    result = response.json()
+    print(f"The probability that this client will get a subscription is: {result['probability']:.4f}")
+else:
+    print(f"Error: {response.status_code}, {response.text}")
