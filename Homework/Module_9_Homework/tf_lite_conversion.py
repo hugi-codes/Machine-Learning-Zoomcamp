7f6bd3350adcf21ec79391fab303e0939215b516
hugi-codes <mindthehug@googlemail.com>
2024-12-11 15:04:31 +0000

    added code for question 2

--- Homework/Module_9_Homework/tf_lite_conversion.py
+++ Homework/Module_9_Homework/tf_lite_conversion.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import os
 
+# Code for Question 1 (converting model to tf lite)
+
 # Correct path to your Keras model
 keras_model_path = "/home/timhug/Machine-Learning-Zoomcamp/Homework/Module_9_Homework"  # adapt path as necessary. Download model as per homework instructions
 
@@ -19,3 +21,25 @@ with open(tflite_model_path, "wb") as f:
 # Get the size of the converted model
 tflite_model_size = os.path.getsize(tflite_model_path)
 print(f"Converted TFLite model size: {tflite_model_size / 1024:.2f} KB")
+
+
+# Code for Question 2 (Output index)
+import tensorflow as tf
+
+# Load the TFLite model
+tflite_model_path = "/home/timhug/Machine-Learning-Zoomcamp/Homework/Module_9_Homework/model_2024_hairstyle.tflite"
+
+# Initialize the TFLite interpreter
+interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
+
+# Allocate tensors (necessary to initialize the model)
+interpreter.allocate_tensors()
+
+# Get the details of the model's input and output
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Print the output index and details
+print(f"Output index: {output_details[0]['index']}")
+print(f"Output name: {output_details[0]['name']}")
+print(f"Output shape: {output_details[0]['shape']}")
